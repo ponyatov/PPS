33c65e8ac1d4585a0201666c7c324f45deb1d162
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-07-25 09:05:28 +0400

    + Android SDK .project
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

---
+++ app/src/main/java/io/github/ponyatov/pps/Console.java
@@ -0,0 +1,13 @@
+package io.github.ponyatov.pps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Console extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_console);
+    }
+}
